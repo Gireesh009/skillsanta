6e49c54dc951b7812816cfa226edc64cea12c2d9
Gireesh009 <68183959+Gireesh009@users.noreply.github.com>
2021-01-19 19:35:42 +0530

    Add files via upload

---
+++ Assignment-1/question2.js
@@ -0,0 +1,12 @@
+var n=15;
+for (var x=0; x<=n; x++) {
+        if (x === 0) {
+                console.log(x +  " is even");
+        }
+        else if (x % 2 === 0) {
+                console.log(x + " is even");   
+        }
+        else {
+                console.log(x + " is odd");
+        }
+}
